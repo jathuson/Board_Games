124c00e2338110b192db3d4acc1749d28fe7bb1a
Jathuson <jathusonj@gmail.com>
2023-04-07 16:03:45 -0400

    game log files

--- game_log_writer.py
+++ game_log_writer.py
@@ -1,5 +1,21 @@
 import csv 
-with open('game_log.csv', 'w', newline='') as file:
-     writer = csv.writer(file)
+def create_game_log():
+     with open('number_game_log.csv', 'w', newline='') as file:
+          writer = csv.writer(file)
 
-     writer.writerow(["Game #", "Winner", "Score"])
+          writer.writerow(["Game #", "Outcome", "Number of Tries"])
+          
+     
+def number_gamelogwriter(guess_count):
+     with open('number_game_log.csv', 'a', newline='') as file:
+          writer = csv.writer(file)
+
+          writer.writerow([2,"Correct", guess_count])
+
+def ttt_gamelogwriter(game_count, player):
+     with open('ttt_game_log.csv', 'a', newline = '') as file:
+          writer = csv.writer(file)
+
+          writer.writerow([game_count, player])
+
+create_game_log()
--- number_game.py
+++ number_game.py
@@ -1,4 +1,5 @@
 import random 
+import game_log_writer
 
 def generate_numbers():
     n = random.randint(0,100+1)
@@ -7,15 +8,19 @@ def generate_numbers():
 
 def play_game():
     g, n = generate_numbers()
+    guess_count = 0
     while True:
         if g < n:
             print(f'Your guess {g} is too low')
+            guess_count += 1
             g= int(float(input('Enter a number for 1 to 100: ')))
         elif g > n :
             print (f'Your guess {g} is too high')
+            guess_count += 1
             g = int(float(input('Enter a number from 1 to 100: ')))
         else:
             print(f'Your guess {g} is right! \nCongratulations!')
-            input()
+            guess_count += 1
+            game_log_writer.gamelogwriter('number_game_log.csv',guess_count)
             break
 
